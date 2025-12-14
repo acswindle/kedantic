6c728a30086e8a9ce54230c63767661d38cde9f6
Chase Swindle <acswindle@gmail.com>
2025-12-14 08:05:00 -0600

    test before node run hook

--- kedantic/plugin.py
+++ kedantic/plugin.py
@@ -16,11 +16,13 @@ class KedanticHook:
         inputs[input_key] = param.annotation(**inputs[input_key])
 
     @hook_impl
-    def before_node_run(self, node: Node, catalog, inputs: dict[str, Any]):
+    def before_node_run(
+        self, node: Node, catalog, inputs: dict[str, Any]
+    ) -> dict[str, Any] | None:
         sig = signature(node.func)
         node_inputs = node._inputs
         if node_inputs is None:
-            return
+            return None
         if isinstance(node_inputs, str):
             node_inputs = [node_inputs]
         if isinstance(node_inputs, list):
